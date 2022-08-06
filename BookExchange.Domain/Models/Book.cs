1c0d54dd10a6d0e6748c5a74fd49ba630b5fd807
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-06 13:43:24 +0300

    Added models

---
+++ BookExchange.Domain/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExchange.Domain.Models
+{
+    public class Book
+    {
+        //public int? ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Image Image { get; set; }
+        public User Owner { get; set; }
+
+        public bool isApproved { get; set; }
+    }
+}
---
+++ BookExchange.Domain/Models/ExchangeOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExchange.Domain.Models
+{
+    public class ExchangeOrder
+    {
+        // public int? ID { get; set; }
+        public string FirstAddress { get; set; }
+        public Book FirstBook { get; set; }
+        public string SecondAddress { get; set; }
+        public Book SecondBook { get; set; }
+
+        public bool IsApproved { get; set; } = false;
+    }
+}
