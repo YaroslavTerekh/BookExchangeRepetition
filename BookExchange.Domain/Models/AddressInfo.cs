6109a69c6cc9dfdd30e93fb11447d879c5c0a957
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-06 20:30:00 +0300

    Modified models
    Modified models, added validation

--- BookExchange.Domain/Models/AddressInfo.cs
+++ BookExchange.Domain/Models/AddressInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace BookExchange.Domain.Models
 {
     public class AddressInfo
     {
+        [Required(ErrorMessage = "Enter your country!")]
         public string Country { get; set; }
+        [Required(ErrorMessage = "Enter your city!")]
         public string City { get; set; }
+        [Required(ErrorMessage = "Enter your post index!")]
         public string PostIndex { get; set; }
     }
 }
