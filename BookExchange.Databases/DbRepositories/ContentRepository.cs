505a6449c718bdfe5a9d1546ce6bd161658f9534
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-09 13:20:15 +0300

    Added repositories

---
+++ BookExchange.Databases/DbRepositories/ContentRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookExchange.Databases.DbContexts;
+using BookExchange.Domain.Models;
+using System.Linq;
+using BookExchange.Databases.DbRepositories.Interfaces;
+
+namespace BookExchange.Databases.DbRepositories
+{
+    public class ContentRepository : IContentRepository
+    {
+        private ContentDbContext _context { get; set; }
+        public ContentRepository (ContentDbContext ctx)
+        {
+            _context = ctx;
+        }
+        //Books
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public void AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public Book GetBook (int id)
+        {
+            return _context.Books.Where(b => b.Id == id).FirstOrDefault();
+        }
+
+        public void DeleteBook(int id)
+        {
+            _context.Books.Remove(GetBook(id));
+            _context.SaveChanges();
+        }
+
+        public void ModifyBook(Book book)
+        {
+            Book originalBook = GetBook(book.Id);
+
+            originalBook.Title = book.Title;
+            originalBook.Description = book.Description;
+            originalBook.Image = book.Image;
+
+            _context.SaveChanges();
+        }
+        //Orders
+        public IEnumerable<ExchangeOrder> GetAllOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public ExchangeOrder GetOrder(int id)
+        {
+            return _context.Orders.Where(o => o.Id == id).FirstOrDefault();
+        }
+
+        public void DeleteOrder(int id)
+        {
+            _context.Orders.Remove(GetOrder(id));
+            _context.SaveChanges();
+        }
+
+        public void ModifyOrder(ExchangeOrder order)
+        {
+            var originalOrder = GetBook(order.Id);
+
+            originalOrder.FirstAddress = order.FirstAddress;
+            originalOrder.SecondAddress = order.SecondAddress;
+            originalOrder.FirstBookId = order.FirstBookId;
+            originalOrder.SecondBookId = order.SecondBookId;
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ BookExchange.Databases/DbRepositories/Interfaces/IContentRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookExchange.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExchange.Databases.DbRepositories.Interfaces
+{
+    public interface IContentRepository
+    {
+        public IEnumerable<Book> GetAllBooks();
+        public Book GetBook(int id);
+        public void DeleteBook(int id);
+        public void ModifyBook(Book book);
+        public IEnumerable<ExchangeOrder> GetAllOrders();        
+        public ExchangeOrder GetOrder(int id);
+        public void DeleteOrder(int id);
+        public void ModifyOrder(ExchangeOrder order);
+    }
+}
