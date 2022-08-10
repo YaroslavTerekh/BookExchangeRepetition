141fc690404fab06ebd5f2277a6de55e5edc7af5
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-10 12:05:30 +0300

    Added depency injection(db contexts)

--- BookExchange.Databases/DbRepositories/Interfaces/IContentRepository.cs
+++ BookExchange.Databases/DbRepositories/Interfaces/IContentRepository.cs
@@ -10,10 +10,12 @@ namespace BookExchange.Databases.DbRepositories.Interfaces
     public interface IContentRepository
     {
         public IEnumerable<Book> GetAllBooks();
+        public void AddBook(Book book);
         public Book GetBook(int id);
         public void DeleteBook(int id);
         public void ModifyBook(Book book);
-        public IEnumerable<ExchangeOrder> GetAllOrders();        
+        public IEnumerable<ExchangeOrder> GetAllOrders();     
+        public void AddOrder(ExchangeOrder order);
         public ExchangeOrder GetOrder(int id);
         public void DeleteOrder(int id);
         public void ModifyOrder(ExchangeOrder order);
