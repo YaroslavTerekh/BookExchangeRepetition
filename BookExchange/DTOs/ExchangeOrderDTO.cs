94526f98f49afc4ea550557ecd8e6a96450be241
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-11 15:33:16 +0300

    Solved problems with Order migration

--- BookExchange/DTOs/ExchangeOrderDTO.cs
+++ BookExchange/DTOs/ExchangeOrderDTO.cs
@@ -3,9 +3,7 @@
     public class ExchangeOrderDTO
     {
         public int Id { get; set; }
-        public AddressInfoDTO FirstAddress { get; set; }
         public BookDTO FirstBook { get; set; }
-        public AddressInfoDTO SecondAddress { get; set; }
         public BookDTO SecondBook { get; set; }
     }
 }
