3b5a6b71fcd0057d5ecd37c453226d2ba74c5667
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-11 11:28:23 +0300

    Added new migration

--- BookExchange/DTOs/ExchangeOrderDTO.cs
+++ BookExchange/DTOs/ExchangeOrderDTO.cs
@@ -3,9 +3,9 @@
     public class ExchangeOrderDTO
     {
         public int Id { get; set; }
-        public string FirstAddress { get; set; }
-        public int FirstBookId { get; set; }
-        public string SecondAddress { get; set; }
-        public int SecondBookId { get; set; }
+        public AddressInfoDTO FirstAddress { get; set; }
+        public BookDTO FirstBook { get; set; }
+        public AddressInfoDTO SecondAddress { get; set; }
+        public BookDTO SecondBook { get; set; }
     }
 }
---
+++ BookExchange/Migrations/20220811082706_ContentOrdersModified.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookExchange.Migrations
+{
+    public partial class ContentOrdersModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SecondAddress",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstAddress",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondAddress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstAddress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
